4846384d8c56784659e58bb908c1ebe68e348967
Emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-03-06 20:43:02 +0000

    Created checkov scan script

--- scripts/terraform_setup.sh
+++ scripts/terraform_setup.sh
@@ -15,7 +15,7 @@ gpg --no-default-keyring \
 --fingerprint
 
 # Add the official HashiCorp Linux repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -op '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 # Update and install Terraform
 sudo apt update
