7de78d262fed3ca50070c4ce94a89fe62c36ddba
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-03-17 23:07:18 +0100

    Updated the S3 bucket name

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "eu-west-s3-bucket"
+    bucket = "my-fullstack-tfstate"
     key    = "pbl-platform-infra/dev/terraform.tfstate"
     region = "us-east-1"
   }
