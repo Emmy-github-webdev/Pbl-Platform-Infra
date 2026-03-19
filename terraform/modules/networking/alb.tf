d918c66ae6984937ab5f3b43fae2ec8ae83b857c
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-03-19 17:45:05 +0100

    Fixed terraform fmt error

--- terraform/modules/networking/alb.tf
+++ terraform/modules/networking/alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "app_alb" {
-  name   = "${var.tags.project}-${var.tags.environment}-app-alb"
+  name               = "${var.tags.project}-${var.tags.environment}-app-alb"
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = aws_subnet.public[*].id
