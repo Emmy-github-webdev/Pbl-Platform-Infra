16adffab910e77bbdce3ddcfdba5e3a779048478
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-03-19 19:24:45 +0100

    Remove the alb for now

---
+++ terraform/modules/networking/kms.tf
@@ -0,0 +1,9 @@
+resource "aws_kms_key" "logs" {
+  description             = "KMS key for CloudWatch Logs"
+  deletion_window_in_days = 7
+}
+
+resource "aws_kms_alias" "logs" {
+  name          = "${var.tags.project}-${var.tags.environment}-alias/cloudwatch-logs"
+  target_key_id = aws_kms_key.logs.key_id
+}
--- terraform/modules/networking/vpc-flow-log.tf
+++ terraform/modules/networking/vpc-flow-log.tf
@@ -1,7 +1,8 @@
 # Cloudwatch log group
 resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
   name              = "/aws/vpc/flow-logs/${var.tags.project}-${var.tags.environment}"
-  retention_in_days = 7
+  retention_in_days = 356
+  kms_key_id = aws_kms_key.logs.arn
 }
 
 resource "aws_iam_role" "flow_logs_role" {
@@ -31,7 +32,15 @@ resource "aws_iam_role_policy" "flow_logs_policy" {
         "logs:CreateLogStream",
         "logs:PutLogEvents"
       ]
-      Resource = "*"
+      Resource = [
+        "${aws_cloudwatch_log_group.vpc_flow_logs.arn}",
+        "${aws_cloudwatch_log_group.vpc_flow_logs.arn}:*"
+      ]
+      # Condition = {
+      #   ArnLike = {
+      #     "aws:SourceArn" = aws_cloudwatch_log_group.vpc_flow_logs.arn
+      #   }
+      # }
     }]
   })
 }
