4a0bb721dfb36ae71477ce629aaa54fe81eda776
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-03-19 19:05:51 +0100

    Remove the alb for now

--- terraform/modules/networking/security-group.tf
+++ terraform/modules/networking/security-group.tf
@@ -40,37 +40,37 @@ resource "aws_security_group" "alb_sg" {
 
 # App SG (private)
 # checkov:skip=CKV2_AWS_5: SG is attached dynamically outside Terraform
-resource "aws_security_group" "pbl_app_sg" {
-  description = "Security group for application backend instances"
-  name        = "${var.tags.project}-${var.tags.environment}-app-sg"
-  vpc_id      = aws_vpc.pbl_vpc.id
+# resource "aws_security_group" "pbl_app_sg" {
+#   description = "Security group for application backend instances"
+#   name        = "${var.tags.project}-${var.tags.environment}-app-sg"
+#   vpc_id      = aws_vpc.pbl_vpc.id
 
-  ingress {
-    description     = "Allow traffic from ALB only"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
+#   ingress {
+#     description     = "Allow traffic from ALB only"
+#     from_port       = 80
+#     to_port         = 80
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.alb_sg.id]
+#   }
 
-  egress {
-    description = "Outbound HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     description = "Outbound HTTPS"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = merge(var.tags, {
-    Name = "${var.tags.project}-${var.tags.environment}-app-sg"
-  })
-}
+#   tags = merge(var.tags, {
+#     Name = "${var.tags.project}-${var.tags.environment}-app-sg"
+#   })
+# }
 
 
 # restrict the default security group
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.pbl_vpc.id
-  
+
   # No ingress rules
 
   # Restrict egress (you can make this even stricter if needed)
--- terraform/modules/networking/waf.tf
+++ terraform/modules/networking/waf.tf
@@ -1,36 +1,36 @@
-resource "aws_wafv2_web_acl" "alb_waf" {
-  name  = "${var.tags.project}-${var.tags.environment}-alb-waf"
-  scope = "REGIONAL"
+# resource "aws_wafv2_web_acl" "alb_waf" {
+#   name  = "${var.tags.project}-${var.tags.environment}-alb-waf"
+#   scope = "REGIONAL"
 
-  default_action {
-    allow {}
-  }
+#   default_action {
+#     allow {}
+#   }
 
-  visibility_config {
-    cloudwatch_metrics_enabled = true
-    metric_name                = "${var.tags.project}-${var.tags.environment}-alb-waf"
-    sampled_requests_enabled   = true
-  }
+#   visibility_config {
+#     cloudwatch_metrics_enabled = true
+#     metric_name                = "${var.tags.project}-${var.tags.environment}-alb-waf"
+#     sampled_requests_enabled   = true
+#   }
 
-  rule {
-    name     = "AWSManagedRulesCommonRuleSet"
-    priority = 1
+#   rule {
+#     name     = "AWSManagedRulesCommonRuleSet"
+#     priority = 1
 
-    override_action {
-      none {}
-    }
+#     override_action {
+#       none {}
+#     }
 
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
-      }
-    }
+#     statement {
+#       managed_rule_group_statement {
+#         name        = "AWSManagedRulesCommonRuleSet"
+#         vendor_name = "AWS"
+#       }
+#     }
 
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "${var.tags.project}-${var.tags.environment}-CommonRuleSet"
-      sampled_requests_enabled   = true
-    }
-  }
-}
+#     visibility_config {
+#       cloudwatch_metrics_enabled = true
+#       metric_name                = "${var.tags.project}-${var.tags.environment}-CommonRuleSet"
+#       sampled_requests_enabled   = true
+#     }
+#   }
+# }
