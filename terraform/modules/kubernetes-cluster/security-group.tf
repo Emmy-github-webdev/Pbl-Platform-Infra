57c5ca7c67438dbfb671eaf261633e383d5ac454
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-03-20 21:25:49 +0100

    Updated the eks cluster and node sg

--- terraform/modules/kubernetes-cluster/security-group.tf
+++ terraform/modules/kubernetes-cluster/security-group.tf
@@ -3,21 +3,6 @@ resource "aws_security_group" "eks_cluster_sg" {
   name        = "${var.tags.project}-${var.tags.environment}-eks-cluster-sg"
   description = "Security group for EKS cluster control plane"
   vpc_id      = var.vpc
-
-  # Allow node groups to communicate with cluster API
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    security_groups = [aws_security_group.eks_nodes_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 # EKS node group security group
@@ -25,28 +10,24 @@ resource "aws_security_group" "eks_nodes_sg" {
   name        = "${var.tags.project}-${var.tags.environment}-eks-nodes-sg"
   description = "Security group for EKS worker nodes"
   vpc_id      = var.vpc
+}
 
-  # Allow nodes to receive traffic from control plane
-  ingress {
-    from_port        = 1025
-    to_port          = 65535
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.eks_cluster_sg.id]
-  }
+ # Allow node groups to communicate with cluster API
+resource "aws_security_group_rule" "nodes_to_cluster" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster_sg.id
+  source_security_group_id = aws_security_group.eks_nodes_sg.id
+}
 
-  # Allow nodes to communicate with each other
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    self        = true
-  }
-
-  # Outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Allow nodes to receive traffic from control plane
+resource "aws_security_group_rule" "cluster_to_nodes" {
+  type                     = "ingress"
+  from_port                = 1025
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_nodes_sg.id
+  source_security_group_id = aws_security_group.eks_cluster_sg.id
 }
